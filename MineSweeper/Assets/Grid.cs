5e00e6febcd624216d613533787adf6239ffb2a0
JherSter <jeroenavis123@gmail.com>
2017-09-15 11:46:31 +0200

    1st push

---
+++ MineSweeper/Assets/Grid.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grid : MonoBehaviour
+{
+
+    public GameObject[,] GridArray = new GameObject[35, 20];
+    [SerializeField] private GameObject _tile, _bomb;
+    [SerializeField] private GameObject _spawner;
+
+    private void Start()
+    {
+        SpawnerGrid();
+    }
+
+    public void SpawnerGrid()
+    {
+
+        for (var x = 0; x < GridArray.GetLength(0); x++)
+        {
+            for (var y = 0; y < GridArray.GetLength(1); y++)
+            {
+
+                GridArray[x, y] = Instantiate(Random.Range(0, 100) < 20 ? _bomb : _tile, new Vector3(x, 0, y),
+                    Quaternion.identity);
+                GridArray[x, y].transform.parent = _spawner.transform;
+
+            }
+
+        }
+    }
+
+   
+}
+
---
+++ MineSweeper/Assets/Player.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.Serialization.Formatters;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Player : MonoBehaviour
+{
+	private Camera _camera;
+	private Grid _grid;
+	
+	
+
+	private void Start()
+	{
+		_camera = GetComponent<Camera>();
+	
+		_grid = GameObject.FindGameObjectWithTag ("GridSpawner").GetComponent<Grid> ();
+	}
+
+	// Update is called once per frame
+	private void Update()
+	{
+
+		var ray = _camera.ScreenPointToRay(Input.mousePosition);
+		if (!Input.GetMouseButtonDown(0)) return;
+		RaycastHit hit;
+		if (!Physics.Raycast(ray, out hit)) return;
+		print("I'm looking at " + hit.transform.name);
+		if (hit.collider.CompareTag ("Bomb"))
+		{
+			StartCoroutine(Restarter());
+
+		}
+		if (hit.collider.CompareTag ("NotABomb"))
+		{
+			print("nee");
+		   //SetActive(false);
+			// voer hier flood fill function uit
+		}
+	}
+
+	private static IEnumerator Restarter()
+	{
+		
+		yield return new WaitForSeconds(5f);
+		SceneManager.LoadScene("Main");
+
+
+	}
+}
